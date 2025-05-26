3b724d59b630f49a44e3cdfca55a825266e483c6
Alma Secerbegovic <alma.secerbegovic@fet.ba>
2025-05-26 23:04:29 +0200

    added backtics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('${process.env.REACT_APP_API_URL}/goals');
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/goals`);
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('${process.env.REACT_APP_API_URL}/goals', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/goals`, {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('${process.env.REACT_APP_API_URL}/goals/' + goalId, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/goals/` + goalId, {
         method: 'DELETE',
       });
 
